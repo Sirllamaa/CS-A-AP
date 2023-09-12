222e1065aeee7e55740961a5e34bfec0b4e1b285
MatthewRichman <61804141+Sirllamaa@users.noreply.github.com>
2023-09-12 11:51:09 -0500

    Update asciiBox2.java
    added header

--- asciiBox2.java
+++ asciiBox2.java
@@ -1,3 +1,14 @@
+/**
+ * asciiBox2.java  09/12/2023
+ *
+ * @author - Matthew Richman
+ * @author - 5
+ * @author - 922408
+ *
+ * @author - I received help from ...
+ *
+*/
+
 public class asciiBox2 {
     public static void main(String[] args) {
         System.out.println("Matt Richman     09/05/23");
