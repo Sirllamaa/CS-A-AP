1de9675d451801990310c1ff69f30ed02f02f4b0
MatthewRichman <61804141+Sirllamaa@users.noreply.github.com>
2023-09-12 11:50:07 -0500

    Update asciiArt1.java
    Added header

--- asciiArt1.java
+++ asciiArt1.java
@@ -1,3 +1,14 @@
+/**
+ * asciiArt1.java  09/12/2023
+ *
+ * @author - Matthew Richman
+ * @author - 5
+ * @author - 922408
+ *
+ * @author - I received help from ...
+ *
+*/
+
 public class asciiArt1 {
     public static void main(String[] args) {
         System.out.println("Matt Richman     09/05/23");
