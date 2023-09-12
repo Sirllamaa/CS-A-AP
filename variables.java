9587b63b2d22d31c5b245633af78f075c39b1714
MatthewRichman <61804141+Sirllamaa@users.noreply.github.com>
2023-09-12 11:52:42 -0500

    Update variables.java
    Added header

--- variables.java
+++ variables.java
@@ -1,3 +1,14 @@
+/**
+ * variables.java  09/12/2023
+ *
+ * @author - Matthew Richman
+ * @author - 5
+ * @author - 922408
+ *
+ * @author - I received help from ...
+ *
+*/
+
 public class variables {
     public static void main(String[] args) {
         
